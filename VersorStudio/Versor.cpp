10fb9c1ef1e2100d3154a7b37cdc9e2a32b95c56
TheZerth <drkainaan@icloud.com>
2024-05-17 00:58:09 -0400

    Extended geometric product and exterior to fully work for all the versor components.

--- VersorStudio/Versor.cpp
+++ VersorStudio/Versor.cpp
@@ -3,8 +3,8 @@
 //
 #include "Versor.h"
 
-static const std::vector<float> e1 = {1.0f,0.0f,0.0f};
-static const std::vector<float> e2 = {0.0f,1.0f,0.0f};
+static const std::vector<float> e1 = {1.0f,0.0f};
+static const std::vector<float> e2 = {0.0f,1.0f};
 Versor Versor::negate() const { return {a * -1.0f, x * -1.0f, y * -1.0f, b * -1.0f}; }
 Versor Versor::reverse() const { return {a, x, y, b * -1.0f}; }
 //Versor Versor::normalize() const;
@@ -13,9 +13,19 @@ Versor Versor::add(const float scalar) const { return {a + scalar, x, y, b}; }
 Versor Versor::sub(const Versor &v) const { return {a - v.a, x - v.x, y - v.y, b - v.b}; }
 Versor Versor::sub(const float scalar) const { return {a - scalar, x, y, b}; }
 Versor Versor::mul(const Versor &v) const {
-    if (v.a != 0.0f) { return {v.a * a,v.a * x,v.a * y,v.a * b};    //If the input versor is a scalar
-    } else if (v.b == 0.0f) { return {0.0f,x * v.x,y * v.y,0.0f};   //If the input versor is a vector
-    } else { return {0.0f,0.0f,0.0f,b * v.b}; } }                   //If the input versor is a bivector
+    float tempa = (a * v.a) + (x * v.x) + (y * v.y) + (-b * v.b);
+    float tempx = (x * v.a) + (b * v.y) + (a * v.x) + (-y * v.b);
+    float tempy = (a * v.y) + (x * v.b) + (y * v.a) + (-b * v.x);
+    float tempb = (b * v.a) + (a * v.b) + (x * v.y) + (-y * v.x);
+    return { tempa, tempx, tempy, tempb };
+}
+Versor Versor::mul(const std::vector<float> &v) const {
+    float tempa = (x * v.x) + (y * v.y);
+    float tempx = (b * v.y) + (a * v.x);
+    float tempy = (a * v.y) + (-b * v.x);
+    float tempb = (x * v.y) + (-y * v.x);
+    return { tempa, tempx, tempy, tempb };
+}
 Versor Versor::mul(const float scalar) const { return {a * scalar, x * scalar, y * scalar, b * scalar}; }
 Versor Versor::div(const Versor &v) const {
     if (v.a != 0.0f) { return {a / v.a,x / v.a,y / v.a,b / v.a};    //If the input versor is a scalar
@@ -24,23 +34,29 @@ Versor Versor::div(const Versor &v) const {
 Versor Versor::div(const float scalar) const { return {a / scalar, x / scalar, y / scalar, b / scalar}; }
 Versor Versor::inr(const Versor &v) const { return {a * v.a + x * v.x + y * v.y - b * v.b, 0.0f, 0.0f, 0.0f}; }
 Versor Versor::inr(const float scalar) const { return { a * scalar, 0.0f, 0.0f, 0.0f}; }
-Versor Versor::ext(const Versor &v) const { return {0.0f, 0.0f, 0.0f, x * v.y - y * v.x}; }
-Versor Versor::ext(const float scalar) const { return {0.0f, 0.0f, 0.0f, 0.0f}; }
+Versor Versor::ext(const Versor &v) const {
+    float tempa = (a * v.a);
+    float tempx = (x * v.a) + (a * v.x);
+    float tempy = (a * v.y) + (y * v.a);
+    float tempb = (b * v.a) + (a * v.b) + (x * v.y) + (-y * v.x);
+    return { tempa, tempx, tempy, tempb };
+}
+Versor Versor::ext(const float scalar) const { return {a * scalar, x * scalar, y * scalar, b * scalar}; }
 Versor Versor::lco(const Versor &v) const {
-    float tempa = a * v.a + x * v.x + y * v.y - b * v.b;
-    float tempx = a * v.x - y * v.b;
-    float tempy = a * v.y + x * v.b;
-    float tempb = a * v.b;
+    float tempa = (a * v.a) + (x * v.x) + (y * v.y) + (-b * v.b);
+    float tempx = (a * v.x) + (-y * v.b);
+    float tempy = (a * v.y) + (x * v.b);
+    float tempb = (a * v.b);
     return { tempa, tempx, tempy, tempb };
 }
 Versor Versor::lco(const float scalar) const {
     return { a * scalar, 0.0f, 0.0f, 0.0f };
 }
 Versor Versor::rco(const Versor &v) const {
-    float tempa = a * v.a + x * v.x + y * v.y - b * v.b;
-    float tempx = x * v.a + b * v.y;
-    float tempy = y * v.a - b * v.x;
-    float tempb = b * v.a;
+    float tempa = (a * v.a) + (x * v.x) + (y * v.y) + (-b * v.b);
+    float tempx = (x * v.a) + (b * v.y);
+    float tempy = (y * v.a) + (-b * v.x);
+    float tempb = (b * v.a);
     return { tempa, tempx, tempy, tempb };
 }
 Versor Versor::rco(const float scalar) const {
